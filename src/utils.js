6b2e0aa978dd4ddecc37ae2bb1c50be522d23da0
Alexandre Abadie <alexandre.abadie@inria.fr>
2023-02-02 16:53:46 +0100

    Job: JobItem: display a precise duration on finished jobs

---
+++ src/utils.js
@@ -0,0 +1,16 @@
+import moment from 'moment';
+
+export const preciseDuration = (value) => {
+  const duration = moment.duration(value * 1000);
+  const seconds = duration.seconds();
+  const minutes = duration.minutes();
+  const hours = duration.hours();
+  const day = duration.days();
+
+  const sDay = day > 0 ? `${day}d`: "";
+  const sHours = hours > 0 ? ((hours < 10) ? `0${hours}h` : `${hours}h`) : "";
+  const sMinutes = minutes > 0 ? ((minutes < 10) ? `0${minutes}m` : `${minutes}m`) : "";
+  const sSeconds = (seconds < 10) ? `0${seconds}s` : `${seconds}s`;
+
+  return `${sDay}${sHours}${sMinutes}${sSeconds}`
+};
